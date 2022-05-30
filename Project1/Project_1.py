4e50dddced871dfd62dcf6caee196d0969fb9700
HJ <hkang07@gmail.com>
2022-05-30 22:38:50 +0100

    comment added

--- Project1/Project_1.py
+++ Project1/Project_1.py
@@ -1,15 +1,20 @@
+# Project 1 required tasks
 import random
 import requests
 
 
 def get_stats():
+    # a random integer between 1-151 generated as a pokemon ID
     ID = random.randint(1, 151)
 
+    # pokeapi address correponding to the ID
     url = 'https://pokeapi.co/api/v2/pokemon/{}'.format(ID)
 
+    # json file saved in pokemon variable
     response = requests.get(url)
     pokemon = response.json()
 
+    # pokemon stats pulled out from jason and saved in and returned as stats dictionary an
     stats = {
         'name': pokemon['name'],
         'id': pokemon['id'],
@@ -21,16 +26,20 @@ def get_stats():
 
 
 def run():
+    # getting stats for player and opponent
     player = get_stats()
     print("Your Pokemon is chosen: {}".format(player['name']))
     opponent = get_stats()
     print("Opponent's Pokemon is chosen: {}".format(opponent['name']))
 
+    # stat chosen by player stored in stat_choice
     stat_choice = input("\nWhich stat do you want to choose? (id, weight, height) ").lower()
 
+    # showing chosen stat's values for player and opponent
     print("Your Pokemon stats: {}".format(player[stat_choice]))
     print("Opponent's Pokemon stats: {}".format(opponent[stat_choice]))
 
+    # Deciding who's stat wins
     if player[stat_choice] > opponent[stat_choice]:
         print("You won!")
     elif player[stat_choice] < opponent[stat_choice]:
